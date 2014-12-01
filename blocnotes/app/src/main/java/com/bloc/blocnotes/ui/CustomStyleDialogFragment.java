fe25801008a7185ef33de29ac9e67ef9775ad5f0
Matt Arnold <matt.jahnold@googlemail.com>
2014-12-01 22:13:17 +0000

    Spinners.  Added a system font spinner to the dialog.

--- blocnotes/app/src/main/java/com/bloc/blocnotes/ui/CustomStyleDialogFragment.java
+++ blocnotes/app/src/main/java/com/bloc/blocnotes/ui/CustomStyleDialogFragment.java
@@ -8,6 +8,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.bloc.blocnotes.R;
 
@@ -29,11 +31,23 @@ public class CustomStyleDialogFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
 
+        // inflate the dialog box and keep a reference to it
+        View view = inflater.inflate(R.layout.fragment_custom_style_dialog, container, false);
+
         // set the dialog title
         getDialog().setTitle("Customize View");
 
+        // set the values in the font spinner
+        Spinner spinner = (Spinner) view.findViewById(R.id.system_font_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this.getActivity(),
+                R.array.system_fonts,
+                android.R.layout.simple_spinner_item
+        );
+        spinner.setAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_style_dialog, container, false);
+        return view;
 
     }
 
